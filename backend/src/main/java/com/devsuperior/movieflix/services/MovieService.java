754221c1a8b80ee9661487782e527b17e117c5e9
Leonardo <leomeireles66@gmail.com>
2025-03-01 11:43:47 -0300

    Movie findById implemented and work with sucessfully

---
+++ backend/src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -0,0 +1,31 @@
+package com.devsuperior.movieflix.services;
+
+import com.devsuperior.movieflix.dto.MovieDetailsDTO;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository repository;
+
+
+    @Transactional(readOnly = true)
+    public MovieDetailsDTO findById(Long id){
+        Optional<Movie> movie = repository.findById(id);
+
+        if(movie.isEmpty()){
+            throw new ResourceNotFoundException("Movie not found");
+        }
+        return new MovieDetailsDTO(movie.get());
+    }
+
+
+}
